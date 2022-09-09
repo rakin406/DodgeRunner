f52e733805adf62a354f2f63f92e793c57a5eb46
Rakin Rahman <rakinhacker@gmail.com>
2022-09-09 22:24:23 +0600

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,36 @@
+#include "raylib.h"
+
+// Program main entry point
+int main()
+{
+    // Initialization
+    const int screenWidth = 800;
+    const int screenHeight = 450;
+
+    InitWindow(screenWidth, screenHeight,
+               "raylib [core] example - basic window");
+
+    SetTargetFPS(60);
+
+    // Main game loop
+    while (!WindowShouldClose())
+    {
+        // Update
+        // TODO: Update your variables here
+
+        // Draw
+        BeginDrawing();
+
+        ClearBackground(RAYWHITE);
+
+        DrawText("Congrats! You created your first window!", 190, 200, 20,
+                 LIGHTGRAY);
+
+        EndDrawing();
+    }
+
+    // De-Initialization
+    CloseWindow();
+
+    return 0;
+}
