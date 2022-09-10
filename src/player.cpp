86a2c83ab3475ed3e32d0f9130500879c1e9375e
Rakin Rahman <rakinhacker@gmail.com>
2022-09-10 18:34:11 +0600

    Implement constructor

--- src/player.cpp
+++ src/player.cpp
@@ -1,6 +1,19 @@
 #include "../include/player.h"
 #include "raylib.h"
 
+Player::Player()
+{
+    Vector3 startingPosition = {0.0F, 0.0F, 0.0F};
+    this->setPlayerPosition(startingPosition);
+}
+
+void Player::moveForward()
+{
+    Vector3 newPosition = this->getPlayerPosition();
+    newPosition.z -= 0.1;
+    this->setPlayerPosition(newPosition);
+}
+
 void Player::draw()
 {
     DrawCube(this->getPlayerPosition(), 2.0F, 2.0F, 2.0F, BLUE);
