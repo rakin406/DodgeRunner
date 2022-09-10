b898c99d633ca4e8cd5e117e6772e59bb9474ecb
Rakin Rahman <rakinhacker@gmail.com>
2022-09-10 17:27:27 +0600

    Write draw method

--- src/player.cpp
+++ src/player.cpp
@@ -1,1 +1,8 @@
 #include "../include/player.h"
+#include "raylib.h"
+
+void Player::draw()
+{
+    DrawCube(Player::getPlayerPosition(), 2.0F, 2.0F, 2.0F, RED);
+    DrawCubeWires(Player::getPlayerPosition(), 2.0F, 2.0F, 2.0F, MAROON);
+}
