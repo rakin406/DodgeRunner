f0bce6ae730188ca482168ddb47eb698e557b77e
Rakin Rahman <rakinhacker@gmail.com>
2022-09-10 17:28:01 +0600

    Create getters and setters for player position

--- include/player.h
+++ include/player.h
@@ -1,10 +1,23 @@
 #ifndef PLAYER_H
 #define PLAYER_H
 
+#include "raylib.h"
+
 class Player
 {
 public:
-    void do_something();
+    Player() { cubePosition = {0.0F, 0.0F, 0.0F}; }
+
+    // Set player position
+    void setPlayerPosition(Vector3 newPosition) { cubePosition = newPosition; }
+
+    // Get player position
+    Vector3 getPlayerPosition() { return cubePosition; }
+
+    void draw();
+
+private:
+    Vector3 cubePosition{};
 };
 
 #endif
