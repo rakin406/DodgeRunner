0438514b4cf944d8c507783e559ead9c4f745a1f
Rakin Rahman <rakinhacker@gmail.com>
2022-09-10 18:18:21 +0600

    Use this pointer

--- src/player.cpp
+++ src/player.cpp
@@ -3,6 +3,6 @@
 
 void Player::draw()
 {
-    DrawCube(Player::getPlayerPosition(), 2.0F, 2.0F, 2.0F, BLUE);
-    DrawCubeWires(Player::getPlayerPosition(), 2.0F, 2.0F, 2.0F, BLACK);
+    DrawCube(this->getPlayerPosition(), 2.0F, 2.0F, 2.0F, BLUE);
+    DrawCubeWires(this->getPlayerPosition(), 2.0F, 2.0F, 2.0F, BLACK);
 }
