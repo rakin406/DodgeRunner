19bf5043b58c8f902249bd9e17edaf49068e342f
Rakin Rahman <rakinhacker@gmail.com>
2022-09-11 13:54:38 +0600

    Add doxygen style comments

--- include/player.h
+++ include/player.h
@@ -9,10 +9,18 @@ class Player
 public:
     Player();
 
-    // Set player position
+    /**
+     * Set player position.
+     *
+     * @param newPosition New position of player cube.
+     */
     void setPlayerPosition(Vector3 newPosition) { position = newPosition; }
 
-    // Get player position
+    /**
+     * Get player cube position.
+     *
+     * @return player cube position.
+     */
     Vector3 getPlayerPosition() { return position; }
 
     // TODO: Remove this function and change it to moveLeft() and moveRight()
