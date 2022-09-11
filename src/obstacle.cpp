0a3d2bbdd45e0b2148d50987e88f3397ce54e922
Rakin Rahman <rakinhacker@gmail.com>
2022-09-11 10:01:08 +0600

    create moveTowardsViewer() function

--- src/obstacle.cpp
+++ src/obstacle.cpp
@@ -1,1 +1,21 @@
 #include "../include/obstacle.h"
+#include "raylib.h"
+
+Obstacle::Obstacle()
+{
+    Vector3 startingPosition = {0.0F, 0.0F, 0.0F};
+    this->setObstaclePosition(startingPosition);
+}
+
+void Obstacle::moveTowardsViewer()
+{
+    Vector3 newPosition = this->getObstaclePosition();
+    newPosition.z += 0.1F;
+    this->setObstaclePosition(newPosition);
+}
+
+void Obstacle::draw()
+{
+    DrawCube(this->getObstaclePosition(), 2.0F, 2.0F, 2.0F, RED);
+    DrawCubeWires(this->getObstaclePosition(), 2.0F, 2.0F, 2.0F, BLACK);
+}
