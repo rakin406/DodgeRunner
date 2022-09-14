37af08f349856321df93e19feeef5963a936ec5b
Rakin Rahman <rakinhacker@gmail.com>
2022-09-15 05:53:32 +0600

    Add world.h and its implementation

---
+++ include/world.h
@@ -0,0 +1,22 @@
+#ifndef WORLD_H
+#define WORLD_H
+
+#include "raylib.h"
+#include <vector>
+
+#include "constants.h"
+#include "menu.h"
+#include "obstacle.h"
+#include "player.h"
+#include "utils.h"
+
+// This class contains the main functions of the game
+class World
+{
+public:
+    World();
+
+private:
+};
+
+#endif
