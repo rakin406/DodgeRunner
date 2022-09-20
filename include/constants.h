1ab6fde95dcf9eb308498d684b509a6723670fad
Rakin Rahman <rakinhacker@gmail.com>
2022-09-20 23:32:03 +0600

    const to constexpr

--- include/constants.h
+++ include/constants.h
@@ -4,39 +4,39 @@
 #include "raylib.h"
 #include <array>
 
-const float CUBE_SIZE = 2.0F;
-const int FONT_SIZE = 30;
+constexpr float CUBE_SIZE = 2.0F;
+constexpr int FONT_SIZE = 30;
 
-const int DEFAULT_OBSTACLES = 10;
+constexpr int DEFAULT_OBSTACLES = 10;
 
 // After the specified score, increase the obstacle speed.
-const int SCORE_INCREMENT = 5000;
+constexpr int SCORE_INCREMENT = 5000;
 
 // Used for incrementing to the original speed
-const float SPEED_INCREMENT = 0.5F;
+constexpr float SPEED_INCREMENT = 0.5F;
 
 namespace screen
 {
-const int WIDTH = 1000;
-const int HEIGHT = 750;
-const char TITLE[] = "Dodge Runner";
+constexpr int WIDTH = 1000;
+constexpr int HEIGHT = 750;
+constexpr char TITLE[] = "Dodge Runner";
 } // namespace screen
 
 namespace camera
 {
-const Vector3 POSITION = {0.0F, 5.0F, 10.0F};
-const float FOV = 45.0F;
+constexpr Vector3 POSITION = {0.0F, 5.0F, 10.0F};
+constexpr float FOV = 45.0F;
 } // namespace camera
 
 namespace ground
 {
-const float GAP = CUBE_SIZE / 2;
-const float WIDTH = (CUBE_SIZE * 3.5F) + GAP;
-const Vector2 SIZE = {WIDTH, static_cast<float>(screen::HEIGHT)};
+constexpr float GAP = CUBE_SIZE / 2;
+constexpr float WIDTH = (CUBE_SIZE * 3.5F) + GAP;
+constexpr Vector2 SIZE = {WIDTH, static_cast<float>(screen::HEIGHT)};
 } // namespace ground
 
 // The middle row position is 0.0F
-const std::array<float, 3> ROW_POSITIONS = {-CUBE_SIZE - ground::GAP / 2, 0.0F,
-                                            CUBE_SIZE + ground::GAP / 2};
+constexpr std::array<float, 3> ROW_POSITIONS = {
+    -CUBE_SIZE - ground::GAP / 2, 0.0F, CUBE_SIZE + ground::GAP / 2};
 
 #endif
